1154c3e8b19c116b3648b0a6a1357f4d77d79d87
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-03-27 22:56:14 +0600

    changes in data

---
+++ src/components/StudentPortal/Login/Login.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <section className="py-6 bg-primary h-screen grid place-items-center">
+                <div className="mx-auto max-w-md px-5 lg:px-0">
+                    <div>
+                        <img className="h-12 mx-auto" src="../assets/image/learningportal.svg" />
+                        <h2 className="mt-6 text-center text-3xl font-extrabold text-slate-100">
+                            Sign in to Student Account
+                </h2>
+                    </div>
+                    <form className="mt-8 space-y-6" action="#" method="POST">
+                        <input type="hidden" name="remember" value="true" />
+                        <div className="rounded-md shadow-sm -space-y-px">
+                            <div>
+                                <label for="email-address" className="sr-only">Email address</label>
+                                <input id="email-address" name="email" type="email" autocomplete="email" required
+                                    className="login-input rounded-t-md" placeholder="Email address" />
+                            </div>
+                            <div>
+                                <label for="password" className="sr-only">Password</label>
+                                <input id="password" name="password" type="password" autocomplete="current-password" required
+                                    className="login-input rounded-b-md" placeholder="Password" />
+                            </div>
+                        </div>
+
+                        <div className="flex items-center justify-end">
+                            <div className="text-sm">
+                                <a href="./StudentReistration.html" className="font-medium text-violet-600 hover:text-violet-500">
+                                    Create New Account
+                        </a>
+                            </div>
+                        </div>
+
+                        <div>
+                            <button type="submit"
+                                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500">
+                                Sign in
+                    </button>
+                        </div>
+                    </form>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <div>
+            {/* <!-- Navigatin Bar. It contains Logo, Center Text And Save Progress Button at the end --> */}
+            <nav className="shadow-md">
+                <div className="max-w-7xl px-5 lg:px-0 mx-auto flex justify-between py-3">
+                    <img className="h-10" src="./assets/image/learningportal.svg" />
+                </div>
+            </nav>
+
+            <section className="py-6 bg-primary">
+                <div className="mx-auto max-w-7xl px-5 lg:px-0 ">
+                    <h1 className="text-4xl font-bold my-4 text-center">You can go to other pages from here</h1>
+
+                    <div className="grid grid-cols-2 gap-5 mt-8">
+                        <div className="bg-slate-900 p-4 border border-slate-700/80 rounded-md">
+                            <h1 className="text-slate-100 font-bold text-xl">Student Portal</h1>
+                            <div className="space-y-2 mt-4 flex flex-col">
+                                <a className="link" href="./StudentPortal/Course Player.html">Course Player</a>
+                                <a className="link" href="./StudentPortal/Leaderboard.html">Leaderboard</a>
+                                <a className="link" href="./StudentPortal/Quiz.html">Quiz</a>
+                                <Link className="link" to="/StudentPortal/login">StudentLogin</Link>
+                                <a className="link" href="./StudentPortal/StudentReistration.html">StudentReistration</a>
+                            </div>
+                        </div>
+                        <div className="bg-slate-900 p-4 border border-slate-700/80 rounded-md">
+                            <h1 className="text-slate-100 font-bold text-xl">Admin Portal</h1>
+                            <div className="space-y-2 mt-4 flex flex-col">
+                                <a className="link" href="./Dashboard/Dashboard.html">Dashboard</a>
+                                <a className="link" href="./Dashboard/AdminLogin.html">AdminLogin</a>
+                                <a className="link" href="./Dashboard/Assignment.html">Assignment</a>
+                                <a className="link" href="./Dashboard/AssignmentMark.html">AssignmentMark</a>
+                                <a className="link" href="./Dashboard/Quizzes.html">Quizzes</a>
+                                <a className="link" href="./Dashboard/Videos.html">Videos</a>
+                            </div>
+                        </div>
+
+                        <div className="bg-slate-900 p-4 border border-slate-700/80 rounded-md">
+                            <h1 className="text-slate-100 font-bold text-xl">Admin Email</h1>
+                            <div className="space-y-2 mt-4 flex flex-col">
+                                <p>Email : admin@learnwithsumit.com</p>
+                                <p>Password : lws@123456</p>
+                            </div>
+                        </div>
+
+                        <div className="bg-slate-900 p-4 border border-slate-700/80 rounded-md">
+                            <h1 className="text-slate-100 font-bold text-xl">Student Email</h1>
+                            <div className="space-y-2 mt-4 flex flex-col">
+                                <p>Email : akash.ahmed@learnwithsumit.com</p>
+                                <p>Email : md.salahuddin@learnwithsumit.com</p>
+                                <p>Email : ferdous.shohag@learnwithsumit.com</p>
+                                <p>Email : riyadh.vai@learnwithsumit.com</p>
+                                <p>Email : saad.hasan@learnwithsumit.com</p>
+                                <p>Password : lws@123456</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Home;
