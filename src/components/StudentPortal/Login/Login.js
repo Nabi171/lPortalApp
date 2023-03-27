692c9f20562e5117fc76a13042fc5fb365386fa1
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-03-27 23:12:45 +0600

    changes inta

--- src/components/StudentPortal/Login/Login.js
+++ src/components/StudentPortal/Login/Login.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from "./learningportal.svg"
+import { Link } from 'react-router-dom';
 const Login = () => {
     return (
         <div>
             <section className="py-6 bg-primary h-screen grid place-items-center">
                 <div className="mx-auto max-w-md px-5 lg:px-0">
                     <div>
-                        <img className="h-12 mx-auto" src={logo} />
+                        <Link to='/'> <img className="h-12 mx-auto" src={logo} /></Link>
                         <h2 className="mt-6 text-center text-3xl font-extrabold text-slate-100">
                             Sign in to Student Account
                 </h2>
