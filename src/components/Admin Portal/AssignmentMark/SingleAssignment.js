3f263cc21172f5a0634f881c99c9447dc3523dfe
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-03-28 23:41:41 +0600

    sldflsdafl

--- src/components/Admin Portal/AssignmentMark/SingleAssignment.js
+++ src/components/Admin Portal/AssignmentMark/SingleAssignment.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
-const SingleAssignment = () => {
+const SingleAssignment = ({ assignment }) => {
+    const { student_name, title, createdAt, totalMark, repo_link } = assignment;
     return (
         <tr>
-            <td className="table-td">Assignment 1 - Implement Debounce Function</td>
-            <td className="table-td">10 Mar 2023 10:58:13 PM</td>
-            <td className="table-td">Saad Hasan</td>
-            <td className="table-td">https://github.com/Learn-with-Sumit/assignment-1</td>
+            <td className="table-td">{title}</td>
+            <td className="table-td">{createdAt}</td>
+            <td className="table-td">{student_name}</td>
+            <td className="table-td">{repo_link}</td>
             {/* <td className="table-td input-mark">
                 <input max="100" value="100" />
                 <svg fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
@@ -15,7 +15,7 @@ const SingleAssignment = () => {
                         d="M4.5 12.75l6 6 9-13.5" />
                 </svg>
             </td> */}
-            <td className="table-td">50</td>
+            <td className="table-td">{totalMark}</td>
 
         </tr>
 
