fe1e72c88cf0ac2d0ad737b445b43bb192de30ba
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-03-29 00:49:32 +0600

    sldffl

--- src/components/Admin Portal/AdminLogin/AdminLogin.js
+++ src/components/Admin Portal/AdminLogin/AdminLogin.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from "../../learningportal.svg";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useLoginMutation } from '../../../features/auth/authApi';
 const AdminLogin = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const [login, { data, isLoading, error: responseError }] =
+        useLoginMutation();
+
+    const navigate = useNavigate();
+    // admin@learnwithsumit.com
+    // lws@123456
+    useEffect(() => {
+        if (responseError ?.data) {
+            setError(responseError.data);
+        }
+        if (data ?.accessToken && data ?.user) {
+            navigate("/Dashboard/dashboard");
+        }
+    }, [data, responseError, navigate]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setError("");
+
+        login({
+            email,
+            password,
+        });
+    };
     return (
         <section className="py-6 bg-primary h-screen grid place-items-center">
             <div className="mx-auto max-w-md px-5 lg:px-0">
@@ -11,17 +41,29 @@ const AdminLogin = () => {
                         Sign in to Admin Account
                 </h2>
                 </div>
-                <form className="mt-8 space-y-6" action="#" method="POST">
-                    <input type="hidden" name="remember" value="true" />
+                <form
+                    onSubmit={handleSubmit}
+                    className="mt-8 space-y-6" action="#" method="POST">
+                    {/* <input
+
+                        type="hidden" name="remember" value="true" /> */}
                     <div className="rounded-md shadow-sm -space-y-px">
                         <div>
                             <label for="email-address" className="sr-only">Email address</label>
-                            <input id="email-address" name="email" type="email" autocomplete="email" required
+                            <input
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                id="email-address" name="email" type="email" autocomplete="email" required
                                 className="login-input rounded-t-md" placeholder="Email address" />
                         </div>
                         <div>
                             <label for="password" className="sr-only">Password</label>
-                            <input id="password" name="password" type="password" autocomplete="current-password" required
+                            <input
+                                value={password}
+                                onChange={(e) =>
+                                    setPassword(e.target.value)
+                                }
+                                id="password" name="password" type="password" autocomplete="current-password" required
                                 className="login-input rounded-b-md" placeholder="Password" />
                         </div>
                     </div>
