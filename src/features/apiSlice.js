13db94286f3b62f3c534d0f2107d01c946c4546f
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-03-28 16:07:44 +0600

    sldflsdalkasdflafl

--- src/features/apiSlice.js
+++ src/features/apiSlice.js
@@ -4,6 +4,14 @@ export const apiSlice = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
         baseUrl: "http://localhost:9000",
+        prepareHeaders: async (headers, { getState, endpoint }) => {
+            const token = getState() ?.auth ?.accessToken;
+            if (token) {
+                headers.set("Authorization", `Bearer ${token}`);
+            }
+
+            return headers;
+        },
     }),
     tagTypes: ["Videos"],
     endpoints: (builder) => ({
