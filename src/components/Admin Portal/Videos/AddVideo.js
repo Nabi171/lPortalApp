24e59d3b9b775744c1d959b4d133edb026cf93a0
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-03-29 14:27:46 +0600

    sgghfhgfjjgjhjgxjkl

--- src/components/Admin Portal/Videos/AddVideo.js
+++ src/components/Admin Portal/Videos/AddVideo.js
@@ -1,7 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAddVideoMutation } from '../../../features/apiSlice';
 
 const AddVideo = () => {
+    const [addVideo, { isLoading, isSuccess, isError }] = useAddVideoMutation();
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [url, setUrl] = useState("");
+    const [views, setViews] = useState("");
+    const [duration, setDuration] = useState("");
+    const [createdAt, setCreatedAt] = useState("");
+    const navigate = useNavigate();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addVideo({
+            title,
+            description,
+            url,
+            views,
+            duration,
+            createdAt
+        });
+        navigate('/Dashboard/videos')
+    };
     return (
         <div>
             <section className="py-6 bg-primary h-screen grid place-items-center">
@@ -13,22 +34,25 @@ const AddVideo = () => {
             </h2>
                     </div>
                     <form
-                        //  onSubmit={handleSubmit}
+                        onSubmit={handleSubmit}
                         className="mt-8 space-y-6" action="#" method="POST" >
                         <input type="hidden" name="remember" value="true" />
                         <div className="rounded-md shadow-sm -space-y-px">
                             <div >
                                 <label for="title" className="sr-only">Video Title</label>
                                 <input
-                                    // value={email}
-                                    // onChange={(e) => setEmail(e.target.value)}
+                                    value={title}
+                                    onChange={(e) => setTitle(e.target.value)}
                                     id="title" name="title" type="text" autocomplete="title" required
                                     className="login-input  rounded-t-md" placeholder="Video Title " />
                             </div>
                             <div>
 
 
-                                <textarea name="" id="" cols="30" rows="7"
+                                <textarea
+                                    value={description}
+                                    onChange={(e) => setDescription(e.target.value)}
+                                    name="" id="" cols="30" rows="7"
                                     id="description" name="description"
                                     autocomplete="description" required
                                     className="login-input rounded-t-md" placeholder='Video Description' ></textarea>
@@ -36,32 +60,32 @@ const AddVideo = () => {
                             <div >
                                 <label for="video-url" className="sr-only">Video Url</label>
                                 <input
-                                    // value={email}
-                                    // onChange={(e) => setEmail(e.target.value)}
+                                    value={url}
+                                    onChange={(e) => setUrl(e.target.value)}
                                     id="url" name="url" type="text" autocomplete="url" required
                                     className="login-input  rounded-t-md" placeholder="Video Url " />
                             </div>
                             <div >
                                 <label for="video-Views" className="sr-only">Video Views</label>
                                 <input
-                                    // value={email}
-                                    // onChange={(e) => setEmail(e.target.value)}
+                                    value={views}
+                                    onChange={(e) => setViews(e.target.value)}
                                     id="views" name="views" type="text" autocomplete="views" required
                                     className="login-input  rounded-t-md" placeholder="Video views " />
                             </div>
                             <div >
                                 <label for="video-duration" className="sr-only">Video Duration</label>
                                 <input
-                                    // value={email}
-                                    // onChange={(e) => setEmail(e.target.value)}
+                                    value={duration}
+                                    onChange={(e) => setDuration(e.target.value)}
                                     id="duration" name="duration" type="text" autocomplete="duration" required
                                     className="login-input  rounded-t-md" placeholder="Video duration " />
                             </div>
                             <div >
                                 <label for="createdAt" className="sr-only">Video Created</label>
                                 <input
-                                    // value={email}
-                                    // onChange={(e) => setEmail(e.target.value)}
+                                    value={createdAt}
+                                    onChange={(e) => setCreatedAt(e.target.value)}
                                     id="createdAt" name="createdAt" type="date"
                                     value="2023-04-15"
                                     autocomplete="date" required
