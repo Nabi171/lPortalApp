0e87fc432f6701876c4715b573ea851d01837d8e
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-03-30 12:39:27 +0600

    sgghfhgfjgkl

--- src/components/Admin Portal/Assignment/Addassignment.js
+++ src/components/Admin Portal/Assignment/Addassignment.js
@@ -13,15 +13,15 @@ const Addassignment = () => {
             </h2>
                         </div>
                         <form
-                            onSubmit={handleSubmit}
+                            // onSubmit={handleSubmit}
                             className="mt-8 space-y-6" action="#" method="POST" >
                             <input type="hidden" name="remember" value="true" />
                             <div className="rounded-md shadow-sm -space-y-px">
                                 <div >
                                     <label for="title" className="sr-only">Video Title</label>
                                     <input
-                                        value={title}
-                                        onChange={(e) => setTitle(e.target.value)}
+                                        // value={title}
+                                        // onChange={(e) => setTitle(e.target.value)}
                                         id="title" name="title" type="text" autocomplete="title" required
                                         className="login-input  rounded-t-md" placeholder="Video Title " />
                                 </div>
@@ -29,8 +29,8 @@ const Addassignment = () => {
 
 
                                     <textarea
-                                        value={description}
-                                        onChange={(e) => setDescription(e.target.value)}
+                                        // value={description}
+                                        // onChange={(e) => setDescription(e.target.value)}
                                         name="" id="" cols="30" rows="7"
                                         id="description" name="description"
                                         autocomplete="description" required
@@ -39,32 +39,32 @@ const Addassignment = () => {
                                 <div >
                                     <label for="video-url" className="sr-only">Video Url</label>
                                     <input
-                                        value={url}
-                                        onChange={(e) => setUrl(e.target.value)}
+                                        // value={url}
+                                        // onChange={(e) => setUrl(e.target.value)}
                                         id="url" name="url" type="text" autocomplete="url" required
                                         className="login-input  rounded-t-md" placeholder="Video Url " />
                                 </div>
                                 <div >
                                     <label for="video-Views" className="sr-only">Video Views</label>
                                     <input
-                                        value={views}
-                                        onChange={(e) => setViews(e.target.value)}
+                                        // value={views}
+                                        // onChange={(e) => setViews(e.target.value)}
                                         id="views" name="views" type="text" autocomplete="views" required
                                         className="login-input  rounded-t-md" placeholder="Video views " />
                                 </div>
                                 <div >
                                     <label for="video-duration" className="sr-only">Video Duration</label>
                                     <input
-                                        value={duration}
-                                        onChange={(e) => setDuration(e.target.value)}
+                                        // value={duration}
+                                        // onChange={(e) => setDuration(e.target.value)}
                                         id="duration" name="duration" type="text" autocomplete="duration" required
                                         className="login-input  rounded-t-md" placeholder="Video duration " />
                                 </div>
                                 <div >
                                     <label for="createdAt" className="sr-only">Video Created</label>
                                     <input
-                                        value={createdAt}
-                                        onChange={(e) => setCreatedAt(e.target.value)}
+                                        // value={createdAt}
+                                        // onChange={(e) => setCreatedAt(e.target.value)}
                                         id="createdAt" name="createdAt" type="date"
                                         value="2023-04-15"
                                         autocomplete="date" required
