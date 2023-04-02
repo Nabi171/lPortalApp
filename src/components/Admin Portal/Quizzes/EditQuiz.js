8cb6a36470be234dd3cc6694dbccfa914f5228b8
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-04-02 11:22:25 +0600

    ssdfsdffakl

--- src/components/Admin Portal/Quizzes/EditQuiz.js
+++ src/components/Admin Portal/Quizzes/EditQuiz.js
@@ -3,7 +3,20 @@ import React from 'react';
 const EditQuiz = () => {
     return (
         <div>
+            <div>
+                <section className="py-6 bg-primary h-screen grid place-items-center">
+                    <div className="mx-auto max-w-md px-5 lg:px-0">
+                        <div>
+                            {/* <Link to='/'> <img className="h-12 mx-auto" /></Link> */}
+                            <h2 className="mt-6 text-center text-3xl font-extrabold text-slate-100">
+                                Edit the Video
+            </h2>
+                        </div>
+                        {/* <FormEdit /> */}
 
+                    </div>
+                </section>
+            </div>
         </div>
     );
 };
