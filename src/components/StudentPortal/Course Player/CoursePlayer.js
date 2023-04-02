59ec24332f29a3674e58b340e5245526447bb8ec
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-04-02 17:10:25 +0600

    ssafdasgkl

--- src/components/StudentPortal/Course Player/CoursePlayer.js
+++ src/components/StudentPortal/Course Player/CoursePlayer.js
@@ -85,7 +85,7 @@ const CoursePlayer = () => {
                         <div
                             className="col-span-full lg:col-auto max-h-[570px] overflow-y-auto bg-secondary p-4 rounded-md border border-slate-50/10 divide-y divide-slate-600/30">
                             <SidePlayer />
-                            <SidePlayer />
+
                         </div>
 
                     </div>
