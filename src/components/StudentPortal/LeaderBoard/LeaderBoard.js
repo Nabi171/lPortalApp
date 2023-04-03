68ca7b024f83107b2e2460dcfac93cf19aed944a
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-04-04 02:33:10 +0600

    changes in the site for the chakeljkjsdlfjlkasdjl slkdjljf

--- src/components/StudentPortal/LeaderBoard/LeaderBoard.js
+++ src/components/StudentPortal/LeaderBoard/LeaderBoard.js
@@ -92,7 +92,23 @@ const LeaderBoard = () => {
 
                     <div className="my-8">
                         <h3 className="text-lg font-bold">Top 20 Result</h3>
-                        <LeaderBoardTable />
+                        <table className="text-base w-full border border-slate-600/50 rounded-md my-4">
+                            <thead>
+                                <tr className="border-b border-slate-600/50">
+                                    <th className="table-th !text-center">Rank</th>
+                                    <th className="table-th !text-center">Name</th>
+                                    <th className="table-th !text-center">Quiz Mark</th>
+                                    <th className="table-th !text-center">Assignment Mark</th>
+                                    <th className="table-th !text-center">Total</th>
+                                </tr>
+                            </thead>
+                            <LeaderBoardTable />
+                            <LeaderBoardTable />
+                            <tbody>
+
+                            </tbody>
+                        </table>
+
                     </div>
                 </div>
             </section>
