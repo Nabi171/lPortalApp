3d398fc569b23d6b46b460089dcd9d72f14caffc
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-04-04 02:48:02 +0600

    changes in the site fot j

--- src/components/StudentPortal/LeaderBoard/LeaderBoardTable.js
+++ src/components/StudentPortal/LeaderBoard/LeaderBoardTable.js
@@ -4,7 +4,7 @@ const LeaderBoardTable = ({ fm }) => {
     const { id, student_name, mark } = fm;
     return (
         <tr className="border-b border-slate-600/50">
-            <td className="table-td text-center">4</td>
+            <td className="table-td text-center">1</td>
             <td className="table-td text-center">{student_name}</td>
             <td className="table-td text-center">00</td>
             <td className="table-td text-center">{mark}</td>
