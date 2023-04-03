24ad4a705801c45ea9c4e89c103a56ab553a3e42
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-04-04 02:46:14 +0600

    changes in the site for t j

--- src/components/StudentPortal/LeaderBoard/LeaderBoardTable.js
+++ src/components/StudentPortal/LeaderBoard/LeaderBoardTable.js
@@ -6,9 +6,9 @@ const LeaderBoardTable = ({ fm }) => {
         <tr className="border-b border-slate-600/50">
             <td className="table-td text-center">4</td>
             <td className="table-td text-center">Saad Hasan</td>
-            <td className="table-td text-center">50</td>
-            <td className="table-td text-center">50</td>
-            <td className="table-td text-center">100</td>
+            <td className="table-td text-center">00</td>
+            <td className="table-td text-center">{mark}</td>
+            <td className="table-td text-center">{mark}</td>
         </tr>
     );
 };
