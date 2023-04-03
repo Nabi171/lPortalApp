8852e1fa2874c70ba69cc9e8370a80f6413fd41b
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-04-04 02:38:18 +0600

    changes in the site for the chakeljkjsdlfjlkasdj j

--- src/components/StudentPortal/LeaderBoard/LeaderBoard.js
+++ src/components/StudentPortal/LeaderBoard/LeaderBoard.js
@@ -33,7 +33,7 @@ const LeaderBoard = () => {
         content = filtersMark.map((fm) => <LeaderBoardTable key={fm.id} fm={fm} />)
     }
 
-    // if (!isLoading && !isError && assignmentMark ?.le
+    // if (!isLoading && !isError && assignmentMark 
 
 
 
