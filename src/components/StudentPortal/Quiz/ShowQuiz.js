c3a2a3e51802d56c07cec5b6e0f3ea944f7e66c3
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-04-04 14:43:53 +0600

    ssafpoeinlcmlkaliowupcmlkgjnvla;sjeoiajojvfaskfapoewflkdsajfoilauipoe4rmflj

--- src/components/StudentPortal/Quiz/ShowQuiz.js
+++ src/components/StudentPortal/Quiz/ShowQuiz.js
@@ -30,7 +30,7 @@ const ShowQuiz = ({ quizze }) => {
                             A function that is called after a certain time interval
                        </label> */}
 
-                        {/* <!-- Option 2 --> */}
+
 
 
 
