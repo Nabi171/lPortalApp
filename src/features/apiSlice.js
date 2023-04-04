ee7ecdeafa20141c33ed8286e919cb2c623603a9
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-04-05 00:15:53 +0600

    ssafpoeinlcmlkaliowupcmlkgjnvla;sjeoiajojvfapj

--- src/features/apiSlice.js
+++ src/features/apiSlice.js
@@ -187,6 +187,7 @@ export const {
 
     useGetAssignmentMarkQuery,
     useEditAssignmentMarkMutation,
+    usePostAssignmentMarkMutation,
 
     useGetAssignmentsQuery,
     useGetAssignmentQuery,
