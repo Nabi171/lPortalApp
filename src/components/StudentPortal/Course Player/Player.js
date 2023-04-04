b79bb39c7563aaf4fcd2d7f393a47c7951dc84c1
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-04-04 19:32:04 +0600

    ssafpoeinlcmlkaliowupcmlkgjnvla;sjeoiajojvfasfpj

--- src/components/StudentPortal/Course Player/Player.js
+++ src/components/StudentPortal/Course Player/Player.js
@@ -73,21 +73,21 @@ const Player = () => {
                             {showModal ? (
                                 <>
                                     <div
-                                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                                        className=" justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
                                     >
                                         <div className="relative w-auto my-6 mx-auto max-w-3xl">
                                             {/*content*/}
                                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                                 {/*header*/}
                                                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                                                    <h5 className="text-2xl font-bold">
+                                                    <h5 className="text-1xl font-bold">
                                                         Submit Your Assignment
                                                     </h5>
                                                     <button
                                                         className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                                                         onClick={() => setShowModal(false)}
                                                     >
-                                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl  block outline-none focus:outline-none">
                                                             ×
                                                       </span>
                                                     </button>
@@ -108,7 +108,7 @@ const Player = () => {
                                                     </div>
                                                 </div>
                                                 {/*footer*/}
-                                                <div className="flex items-center justify-end p-6 border-t border-solid gap-4 border-slate-200 rounded-b">
+                                                <div className="mt-1 flex items-center justify-end p-6 border-t border-solid gap-4 border-slate-200 rounded-b">
                                                     <button
                                                         // className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                                         className="px-3 font-bold py-1 border border-cyan text-cyan rounded-full text-sm hover:bg-cyan hover:text-primary"
