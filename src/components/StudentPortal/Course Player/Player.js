0110b0c944a34858c0b0cb46c0fdc2abf4b33fd6
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-04-04 19:16:45 +0600

    ssafpoeinlcmlkaliowupcmlkgjnvla;sjeoiajojvfaskfapoewflspj

--- src/components/StudentPortal/Course Player/Player.js
+++ src/components/StudentPortal/Course Player/Player.js
@@ -109,14 +109,15 @@ const Player = () => {
                                                         type="button"
                                                         onClick={() => setShowModal(false)}
                                                     >
-                                                        Close
+                                                        Cancel
                                                       </button>
                                                     <button
-                                                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                        // className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                        className="px-3 font-bold py-1 border border-cyan text-cyan rounded-full text-sm hover:bg-cyan hover:text-primary"
                                                         type="button"
                                                         onClick={() => setShowModal(false)}
                                                     >
-                                                        Save Changes
+                                                        Submit Assignment
                                                     </button>
                                                 </div>
                                             </div>
