478c6eb9d5344d196afd6c4c2633ecadfc162c50
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-04-04 19:09:47 +0600

    ssafpoeinlcmlkaliowupcmlkgjnvla;sjeoiajojvfaskfapoewflkdsajfpj

--- src/components/StudentPortal/Course Player/Player.js
+++ src/components/StudentPortal/Course Player/Player.js
@@ -46,6 +46,20 @@ const Player = () => {
                 </div>
             </nav>
 
+
+            //show modal
+
+
+
+
+
+
+
+
+
+
+
+
             <section className="py-6 bg-primary">
                 <div className="mx-auto max-w-7xl px-5 lg:px-0">
                     <div className="grid grid-cols-3 gap-2 lg:gap-8">
