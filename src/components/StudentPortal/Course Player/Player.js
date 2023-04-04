c55fd8174e32554b40016d464b08102498d88b9f
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-04-04 19:21:45 +0600

    ssafpoeinlcmlkaliowupcmlkgjnvla;sjeoiajojvfaskfapoewfpj

--- src/components/StudentPortal/Course Player/Player.js
+++ src/components/StudentPortal/Course Player/Player.js
@@ -80,9 +80,9 @@ const Player = () => {
                                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                                 {/*header*/}
                                                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                                                    <h3 className="text-3xl font-semibold">
-                                                        Modal Title
-                                                    </h3>
+                                                    <h5 className="text-2xl font-bold">
+                                                        Submit Your Assignment
+                                                    </h5>
                                                     <button
                                                         className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                                                         onClick={() => setShowModal(false)}
@@ -94,16 +94,10 @@ const Player = () => {
                                                 </div>
                                                 {/*body*/}
                                                 <div className="relative p-6 flex-auto">
-                                                    <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                                                        I always felt like I could do anything. That’s the main
-                                                        thing people are controlled by! Thoughts- their perception
-                                                        of themselves! They're slowed down by their perception of
-                                                        themselves. If you're taught you can’t do anything, you
-                                                        won’t do anything. I was taught I could do everything.
-                                                   </p>
+                                                    <input type="text" />
                                                 </div>
                                                 {/*footer*/}
-                                                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                                <div className="flex items-center justify-end p-6 border-t border-solid gap-4 border-slate-200 rounded-b">
                                                     <button
                                                         // className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                                         className="px-3 font-bold py-1 border border-cyan text-cyan rounded-full text-sm hover:bg-cyan hover:text-primary"
@@ -114,7 +108,7 @@ const Player = () => {
                                                       </button>
                                                     <button
                                                         // className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                                        className="px-3  font-bold py-1 border border-cyan text-cyan rounded-full text-sm hover:bg-cyan hover:text-primary"
+                                                        className="px-3 font-bold py-1 border border-cyan text-cyan rounded-full text-sm hover:bg-cyan hover:text-primary"
                                                         type="button"
                                                         onClick={() => setShowModal(false)}
                                                     >
