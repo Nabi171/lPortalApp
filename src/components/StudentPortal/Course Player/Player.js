ecc6eea9f20067ffc643ac7a30eb053402cde30f
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-04-04 19:17:54 +0600

    ssafpoeinlcmlkaliowupcmlkgjnvla;sjeoiajojvfaskfapoewflpj

--- src/components/StudentPortal/Course Player/Player.js
+++ src/components/StudentPortal/Course Player/Player.js
@@ -105,7 +105,8 @@ const Player = () => {
                                                 {/*footer*/}
                                                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                                                     <button
-                                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                        // className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                        className="px-3 font-bold py-1 border border-cyan text-cyan rounded-full text-sm hover:bg-cyan hover:text-primary"
                                                         type="button"
                                                         onClick={() => setShowModal(false)}
                                                     >
@@ -113,7 +114,7 @@ const Player = () => {
                                                       </button>
                                                     <button
                                                         // className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                                        className="px-3 font-bold py-1 border border-cyan text-cyan rounded-full text-sm hover:bg-cyan hover:text-primary"
+                                                        className="px-3  font-bold py-1 border border-cyan text-cyan rounded-full text-sm hover:bg-cyan hover:text-primary"
                                                         type="button"
                                                         onClick={() => setShowModal(false)}
                                                     >
