3367f78ecee728f173554be51c66ec3c4a319740
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-04-04 14:08:18 +0600

    changes  j

--- src/components/Admin Portal/AssignmentMark/AssignmentMark.js
+++ src/components/Admin Portal/AssignmentMark/AssignmentMark.js
@@ -33,7 +33,7 @@ const AssignmentMark = () => {
 
     const filteredPublish = assignmentMark ?.filter((pb) => pb.status == "published");
 
-    // console.log(filteredP
+    // console.log(filtere
     // }
 
 
