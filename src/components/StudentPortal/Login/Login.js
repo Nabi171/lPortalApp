a69faf8ef41fdd39df329bf5393f324c1d2da567
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-04-07 02:15:28 +0600

    change in repo

--- src/components/StudentPortal/Login/Login.js
+++ src/components/StudentPortal/Login/Login.js
@@ -38,7 +38,7 @@ const Login = () => {
                     <div>
                         <Link to='/'> <img className="h-12 mx-auto" src={logo} /></Link>
                         <h2 className="mt-6 text-center text-3xl font-extrabold text-slate-100">
-                            Sign in to Student Account
+                            Login in to Student Account
                 </h2>
                     </div>
                     <form onSubmit={handleSubmit} className="mt-8 space-y-6" action="#" method="POST" >
